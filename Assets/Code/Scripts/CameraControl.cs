f27979c4b6688166126f7ec4886efe1b6895922d
BitMibi <ethancowan07@gmail.com>
2025-11-05 16:10:57 +0000

    Added Camera Following Player
    Made the camera follow player object whilst moving

---
+++ Assets/Code/Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;       //Object camera is tied to
+    private Vector3 offset;          
+    public Vector3 initialOffset;       //Initial Camera Offset -- Can be changed in engine
+    public Quaternion initialRotation; //Initial Camera Rotation -- Should be able to be changed in engine??
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+           //Sets the initial Rotation and position of the camera.
+        transform.rotation = initialRotation;       
+        transform.position = player.transform.position + initialOffset;
+        
+        //KEEPS the inital position of the camera.
+        offset = transform.position - player.transform.position;        //Distance away from player
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;        // Changes the position to follow the player as tey move
+    }
+
+
+
+
+}
